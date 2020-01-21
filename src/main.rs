fedc237908354ec137038691bbe366c9aa88528a
panguangze <guangzep@gmail.com>
2020-01-21 18:51:48 +0800

    初始化

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![no_std] // 不链接Rust标准库
+#![no_main] // 禁用所有Rust层级的入口点
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2+1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+/// 这个函数将在panic时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
