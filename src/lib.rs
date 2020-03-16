cc426b2cceb526cee2e6b6ab801d49c73b6992cb
panguangze <guangzep@gmail.com>
2020-03-16 16:22:55 +0800

    学习

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![feature(alloc_error_handler)]
+#![feature(abi_x86_interrupt)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
+
+pub mod serial;
+pub mod vga_buffer;
+pub mod interrupts;
+pub mod gdt;
+pub mod memory;
+pub mod allocator;
+
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe{interrupts::PICS.lock().initialize()};
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// Entry point for `cargo xtest`
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+#[no_mangle]
+fn test_kernel_main(boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    caronkey_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub fn hlt_loop() -> !{
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,71 @@
 #![no_std] // 不链接Rust标准库
 #![no_main] // 禁用所有Rust层级的入口点
+#![feature(custom_test_frameworks)]
+#![test_runner(caronkey_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use caronkey_os::println;
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
+use alloc::{boxed::Box, vec, vec::Vec};
 
-static HELLO: &[u8] = b"Hello World!";
+entry_point!(kernel_main);
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use caronkey_os::allocator;
+    use caronkey_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{structures::paging::Page, VirtAddr};
+    println!("Hello World{}", "!");
+    caronkey_os::init();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2+1) = 0xb;
-        }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
     }
+    println!("vec at {:p}", vec.as_slice());
+    
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    loop {}
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    caronkey_os::hlt_loop()
 }
 
 /// 这个函数将在panic时被调用
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    caronkey_os::hlt_loop()
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    caronkey_os::test_panic_handler(info)
 }
